e35c86b67c594c17a6a972db1dff7f1e5855638c
shuinoue <shuinoue0817@gmail.com>
2019-09-17 22:48:14 +0900

    changed namespace in voronoi_map.launch

--- multi_planning_server/src/voronoi_map.cpp
+++ multi_planning_server/src/voronoi_map.cpp
@@ -1,11 +1,14 @@
 #include<multi_planning_server/voronoi_map.hpp>
+#include <exploration_libraly/path_planning.hpp>
+#include <voronoi_planner/planner_core.h>
+#include <navfn/navfn_ros.h>
 
 using std::cout;
 using std::endl;
 
-voronoiMap::voronoiMap(std::string initTopicNameSpace_,std::string initGlobalFrameId_):tf(ros::Duration(10)),globalCostmap("voronoi_map",tf)
+voronoiMap::voronoiMap(std::string initTopicNameSpace_,std::string initGlobalFrameId_):tf(ros::Duration(10)),globalCostmap("global_costmap",tf)
 {
-    name="voronoi_map";
+    name = "global_costmap";
     topicNameSpace_=initTopicNameSpace_;
     globalFrameId_=initGlobalFrameId_;
 }
@@ -57,21 +60,31 @@ int main(int argc, char **argv)
     cout << "voronoi_map started." << endl;
     ros::init(argc, argv, "voronoi_map");
     ros::NodeHandle nh("~");
-    geometry_msgs::PoseStamped start;
-    geometry_msgs::PoseStamped goal;
-    std::vector<geometry_msgs::PoseStamped> plan;
+    // geometry_msgs::PoseStamped start;
+    // geometry_msgs::PoseStamped goal;
+    // std::vector<geometry_msgs::PoseStamped> plan;
     std::string topicNameSpace;
     nh.getParam("namespace",topicNameSpace);
+    cout << topicNameSpace << endl;
     std::string globalFrameId;
-    nh.getParam(topicNameSpace+"/global_frame_id",globalFrameId);
-
+    nh.getParam("global_frame",globalFrameId);
+    cout << globalFrameId << endl;
     ros::NodeHandle n;
     ros::Subscriber sub;
     ros::CallbackQueue queue;
     n.setCallbackQueue(&queue);
-
+    // geometry_msgs::PoseStamped start, goal;
+    // start.header.frame_id = globalFrameId;
+    // goal.header.frame_id=globalFrameId;
+    // std::vector<geometry_msgs::PoseStamped> path;
+    // ExpLib::PathPlanning<navfn::NavfnROS> pp("global_cosmap", "voronoi_planner");
+    // while(ros::ok())
+    // {
+    //     pp.createPath(start, goal, path);
+    // }
     voronoiMap vm(topicNameSpace,globalFrameId);
     sub=n.subscribe("/robot1/move_base/global_costmap/costmap",1,&voronoiMap::mergeMapCallBack,&vm);
+    cout << " etst "<< endl;
     cout << "setting completed." << endl;
     while(ros::ok())
     {
