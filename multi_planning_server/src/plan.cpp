778dde2ec00c4eae593021fdc7db3562df274e3e
shuinoue <shuinoue0817@gmail.com>
2019-10-31 21:24:33 +0900

    intaruppted adding simulator environment

--- multi_planning_server/src/plan.cpp
+++ multi_planning_server/src/plan.cpp
@@ -26,50 +26,7 @@ void plan::robotDataSetter(const exploration_msgs::FrontierArray& frontiers,cons
     int count=0;
     int id=0;
     avoidedTargetCounter=0;
-    /*
-    //test data
-    nav_msgs::Path testPath;
-    nav_msgs::Odometry testOdom;
-    geometry_msgs::PoseStamped testGoal;
-
-    testOdom.pose.pose.position.x=0;
-    testOdom.pose.pose.position.y=0;
-    testGoal.pose.position.x=11;
-    testGoal.pose.position.y=sin(11);
-    testPath.poses.resize(10);
-    for(int i=0;i<10;i++)
-    {
-        testPath.poses[i].pose.position.x=i+1;
-        testPath.poses[i].pose.position.y=sin(i+1);
-    }
-
-    std::random_device rnd;
-    double locationx=0.0;
-    double locationy=0.0;
-    cout << setw(15) << "goalx" << setw(15) << "goaly" << setw(15) << "locationx" <<  setw(15) << "locationy" << setw(15) << "Path" << setw(15) << "id" << endl;
-    
-    for(int i=0; i<numberOfFrontiers; i++)
-    {
-        double rndtergetx,rndtargety;
-        rndtergetx=rnd();
-        rndtargety=rnd();
-        geometry_msgs::PoseStamped tmpgoal;
-        tmpgoal.pose.position.x = rndtergetx;
-        tmpgoal.pose.position.y = rndtargety;
-
-        nav_msgs::Odometry tmplocation;
-        tmplocation.pose.pose.position.x = locationx;
-        tmplocation.pose.pose.position.y = locationy;
-
-        nav_msgs::Path dummyPath;
-
-        robotData tmprobotdata={tmpgoal,tmplocation,dummyPath,getDistance(rndtergetx,rndtargety,locationx,locationy),++id};
-        testRobotData.push_back(tmprobotdata);
-        cout << setw(15) << testRobotData[i].goal.pose.position.x << setw(15) << testRobotData[i].goal.pose.position.y << setw(15) << testRobotData[i].location.pose.pose.position.x <<  setw(15) << testRobotData[i].location.pose.pose.position.y << setw(15) << testRobotData[i].pathLength << setw(15) << testRobotData[i].memberID << endl;
-    }
-    */
     nav_msgs::Odometry tmplocation;
-    //tmplocation = *recievedOdometry;
     tmplocation.pose.pose.position.x = recievedOdometry.pose.pose.position.x;
     tmplocation.pose.pose.position.y = recievedOdometry.pose.pose.position.y;
     geometry_msgs::PoseStamped stampedLocation;
@@ -126,7 +83,6 @@ double plan::calcPathFromLocationToTarget(geometry_msgs::PoseStamped Goal, nav_m
 //パスの情報を距離の長さに変換する関数
 double plan::getDistance(double x, double y, double x2, double y2)
 {
-    // cout << "data : " << x <<" "<<  y <<" "<<  x2 <<" "<< y2 << endl;
     double distance=sqrt(pow(x2-x,2)+pow(y2-y,2));
     return distance;
 }   
@@ -153,7 +109,6 @@ robotData plan::transrateFromCombinatedPathsToRobotData(combinatedPaths_t comb, 
 {
     robotData tmpRobotData;
     tmpRobotData.memberID = comb.chosenID[robotNum-1];
-    //tmpRobotData.pathLength = comb.combinatedPathLength[robotNum -1];
     return tmpRobotData;
 }
 // 各ロボットの各目的地までの距離を組み合わせて合計していく関数
